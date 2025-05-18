fbb15da3251075d7fe8ebf33dd7e58d6acb785c7
Effat-Ara-Daijy <daijyeffat@gmail.com>
2025-05-18 16:18:13 +0600

    updated

--- src/component/TeamMemberCard.jsx
+++ src/component/TeamMemberCard.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 
-const TeamMemberCard = ({id,name,email,address}) => {
+const TeamMemberCard = () => {
+    const teamMember1= [{id: 1, name: "Daisy", email:"daisy@gmail.com", address: "24 north kafrul"},
+                      {id: 2, name: "Humaira", email:"humaira@gmail.com", address: "airport Uttara"},
+                      {id: 3, name: "Ariful", email:"ariful@gmail.com", address: "mirpur 13"},
+  ]
   return (
-    <div className='team-info-card'>
-        <div className="team-info-list">
-            <h1>Name : {name}</h1>
-            <p>ID : {id}</p>
-            <p>Email: {email}</p>
-            <p>Address: {address}</p>
-        </div>
-    </div>
+    <>
+    {teamMember1.map((team)=>{
+      return(
+        <div className='team-info-card'>
+          <div className="team-info-list">
+              <h1>Name : {team.name}</h1>
+              <p>ID : {team.id}</p>
+              <p>Email: {team.email}</p>
+              <p>Address: {team.address}</p>
+          </div>
+      </div>
+      )
+    })}
+
+    </>
+
   )
 }
 
